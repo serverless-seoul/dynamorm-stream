c4fbcbaa29036dadb5dc3425766afa96fd8c9cd7
MooYeol Presott Lee <mooyoul@gmail.com>
2018-05-30 04:11:15 +0900

    lint

--- src/lambda_context.ts
+++ src/lambda_context.ts
@@ -1,26 +1,26 @@
 // Response
 interface Response {
-    statusCode: number;
-    headers: {
-        [key: string]: any;
-        "Content-Type": string;
-    };
-    body: string;
+  statusCode: number;
+  headers: {
+    [key: string]: any;
+    "Content-Type": string;
+  };
+  body: string;
 }
 
 export interface LambdaContext {
-    // Properties
-    functionName: string;
-    functionVersion: string;
-    invokedFunctionArn: string;
-    memoryLimitInMB: number;
-    awsRequestId: string;
-    logGroupName: string;
-    logStreamName: string;
+  // Properties
+  functionName: string;
+  functionVersion: string;
+  invokedFunctionArn: string;
+  memoryLimitInMB: number;
+  awsRequestId: string;
+  logGroupName: string;
+  logStreamName: string;
 
-    // Functions
-    succeed(result?: object): void;
-    fail(error: Error): void;
-    done(error: Error | null, result?: Response): void; // result must be JSON.stringifyable
-    getRemainingTimeInMillis(): number;
+  // Functions
+  succeed(result?: object): void;
+  fail(error: Error): void;
+  done(error: Error | null, result?: Response): void; // result must be JSON.stringifyable
+  getRemainingTimeInMillis(): number;
 }
