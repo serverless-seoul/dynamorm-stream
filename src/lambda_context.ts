72c2b527526cca5fc0df49a96d652981686db136
MooYeol Presott Lee <mooyoul@gmail.com>
2018-05-30 16:00:13 +0900

    remove `Response` interface

--- src/lambda_context.ts
+++ src/lambda_context.ts
@@ -1,13 +1,3 @@
-// Response
-interface Response {
-  statusCode: number;
-  headers: {
-    [key: string]: any;
-    "Content-Type": string;
-  };
-  body: string;
-}
-
 export interface LambdaContext {
   // Properties
   functionName: string;
@@ -21,6 +11,6 @@ export interface LambdaContext {
   // Functions
   succeed(result?: object): void;
   fail(error: Error): void;
-  done(error: Error | null, result?: Response): void; // result must be JSON.stringifyable
+  done(error: Error | null, result?: any): void; // result must be JSON.stringifyable
   getRemainingTimeInMillis(): number;
 }
